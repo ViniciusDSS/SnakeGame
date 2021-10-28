8356e1edc6360ccbe253942b264541be95376844
Vinicius D Silva <viniciusd37@gmail.com>
2021-10-27 23:47:45 -0300

    o primeiro commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let canvas = document.getElementById('snake'); //criar elemento que irá rodar o jogo
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
+snake [0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
+
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillReact(0, 0, 16 * box, 16 * box);//desenha o retângulo usando x e y e a largura e altura setadas
+}
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillReact(snake[i].x,snake[i].y, box, box)
+    }
+}
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+snake.unshift(newHead);
+
+}
+let jogo = setInterval(iniciarJogo, 100);
