e468bdfc2763e3f2e3fd8d3eef9be9c8b27e99e1
Vinicius D Silva <viniciusd37@gmail.com>
2021-10-30 00:02:40 -0300

    2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementById('snake'); //criar elemento que irá rodar o jogo
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
@@ -10,7 +10,7 @@ let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
-    context.fillReact(0, 0, 16 * box, 16 * box);//desenha o retângulo usando x e y e a largura e altura setadas
+    context.fillReact(0, 0, 16*box, 16*box);//desenha o retângulo usando x e y e a largura e altura setadas
 }
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
@@ -18,7 +18,21 @@ function criarCobrinha(){
         context.fillReact(snake[i].x,snake[i].y, box, box)
     }
 }
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].x < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
     let snakeX = snake[0].x;
